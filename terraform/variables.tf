b724c0d6f79a4bed630971cae055aa0281afa409
Eddy Leim <eddyleim@gmail.com>
2025-09-26 22:13:19 +0000

    ci: init no interactivo (-migrate-state) y fix de variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,19 @@
-variable "project_id" {
-  type = string
+
+variable "region" {
+
+  type    = string
+
+  default = "us-central1"
+
 }
-variable "region"    { type = string, default = "us-central1" }
-variable "func_name" { type = string, default = "ec41-uploader" }
+
+
+
+variable "func_name" {
+
+  type    = string
+
+  default = "ec41-uploader"
+
+}
+
