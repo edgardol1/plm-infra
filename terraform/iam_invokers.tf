cefbc04d9e49ef964a8df210b39bb5716b93499e
Edgardo Rojas <edgardo@example.com>
2025-09-28 22:32:01 -0500

    tf: fix duplicate data, use vars; name-only for CFv2/Run

--- terraform/iam_invokers.tf
+++ terraform/iam_invokers.tf
@@ -1,17 +1,15 @@
-data "google_project" "this" {}
-
 resource "google_cloudfunctions2_function_iam_member" "cf_invoker" {
-  project        = data.google_project.this.project_id
+  project        = var.project_id
   location       = var.region
-  cloud_function = "projects/${data.google_project.this.project_id}/locations/${var.region}/functions/${var.func_name}"
+  cloud_function = var.func_name
   role           = "roles/cloudfunctions.invoker"
   member         = "serviceAccount:${var.sa_email}"
 }
 
 resource "google_cloud_run_service_iam_member" "run_invoker" {
-  project  = data.google_project.this.project_id
+  project  = var.project_id
   location = var.region
-  service  = "v1/projects/${data.google_project.this.project_id}/locations/${var.region}/services/${var.func_name}"
+  service  = var.func_name
   role     = "roles/run.invoker"
   member   = "serviceAccount:${var.sa_email}"
 }
