f943726073755a33dc34aabe40b1df079473767f
Edgardo Rojas <edgardo@example.com>
2025-09-28 21:23:16 -0500

    ci: Terraform CI/CD via WIF + vars mínimas

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,2 @@
-variable "region" {
-  type    = string
-  default = "us-central1"
-}
-
-variable "func_name" {
-  type    = string
-  default = "ec41-uploader"
-}
+variable "project_id" { type = string }
+variable "region"     { type = string  default = "us-central1" }
